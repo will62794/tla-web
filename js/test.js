c154a914ceeee010a558ae2bed49e7c703528dee
William Schultz <william.schultz@mongodb.com>
2024-12-24 09:50:06 -0500

    Add simple lazy test

--- js/test.js
+++ js/test.js
@@ -243,6 +243,8 @@ async function testStateGraphEquiv(testId, stateGraph, parsedSpec, specPath, con
         { "spec": "simple_infix_def", "constvals": undefined },
         { "spec": "simple_enabled", "constvals": undefined },
         { "spec": "simple_fcn", "constvals": undefined },
+        // TODO: Re-enable this test once we figure out how to handle lazy evaluation.
+        // { "spec": "simple_lazy", "constvals": undefined },
         { "spec": "simple_subset", "constvals": undefined },
         { "spec": "simple_quant", "constvals": undefined },
         { "spec": "simple_quant2", "constvals": undefined },
